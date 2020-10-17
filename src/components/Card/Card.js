0ad27cb423e66e83f72befb7848e95ee59212b5f
Demeqrosad <demetriusz@kurosad.pl>
2020-10-17 21:34:47 +0200

    Add cards in columns

---
+++ src/components/Card/Card.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from './Card.scss';
+import PropTypes from 'prop-types';
+
+class Card extends React.Component {
+  static propTypes = 
+  {
+    title: PropTypes.string.isRequired,
+  };
+
+  static defaultProps = 
+  {};
+
+  render()
+  {
+    return (
+      <div className={styles.component}>
+        <p>{this.props.title}</p>
+      </div>
+    )
+  };
+}
+
+export default Card;
