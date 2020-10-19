a898e26096488cf4450baa9b1c678c58a28ef56c
Demeqrosad <demetriusz@kurosad.pl>
2020-10-19 23:04:20 +0200

    Enable adding new columns and cards

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -2,8 +2,8 @@ import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
 import Card from '../Card/Card.js';
-//import Creator from '../Creator/Creator.js';
-//import {settings} from '../../data/dataStore';
+import Creator from '../Creator/Creator.js';
+import {settings} from '../../data/dataStore';
 import Icon from '../Icon/Icon.js';
 
 class Column extends React.Component {
@@ -13,13 +13,16 @@ class Column extends React.Component {
     title: PropTypes.string.isRequired,
     icon: PropTypes.string,
     cards: PropTypes.array,
+    addCard: PropTypes.func,
   };
 
-  static defaultProps = {};
+  static defaultProps = {
+    icon: settings.defaultColumnIcon,
+  };
 
   render()
   {
-    const {title, icon, cards} = this.props;
+    const {title, icon, cards, addCard} = this.props;
     return (
       <section className={styles.component}>
         <h3 className={styles.title}>{title}
@@ -30,12 +33,7 @@ class Column extends React.Component {
         {cards.map(cardsData => (
           <Card key={cardsData.id} {...cardsData} />
         ))}
-        {/*
-        {this.state.cards.map(({key, ...cardProps}) => (
-          <Card key={key} {...cardProps} />
-        ))}
-        <Creator text={settings.cardCreatorText} action={title => this.addCard(title)}/>
-      */}
+        <Creator text={settings.cardCreatorText} action={addCard}/>
       </section>
     );
   }
